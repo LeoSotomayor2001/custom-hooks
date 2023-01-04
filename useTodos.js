f4ea69ea89336e4c809e7b67c46b176867165ed2
Leodomi <Leodomi15@gmail.com>
2023-01-04 19:01:52 -0400

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,59 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const init=()=>{
+    //parse es el opuesto de stringify
+    //intenta parsear todo lo que esta en el LS y si  esta vacio agarra el arreglo
+    return JSON.parse(localStorage.getItem('todos') )|| [];
+}
+
+export const useTodos = () => {
+
+    //init inicializa el reducer
+    const [todos, dispatch] = useReducer(todoReducer, [],init);
+
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todos));
+    
+      
+    }, [todos])
+    
+
+    const handleNewTodo=(todo)=>{
+        const action={
+            type:'[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo=(id)=>{
+        dispatch({
+            type:'[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo=(id)=>{
+        dispatch({
+            type:'[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+
+    return{
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo,
+        todos,
+        todosCount:todos.length,
+        pendingTodosCount:todos.filter(todo=> !todo.done).length
+    }
+
+
+
+
+  
+}
